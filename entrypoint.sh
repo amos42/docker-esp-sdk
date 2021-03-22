5823e9e59b57c24cdd2d947d5d6ee2d8ab80bf56
amos42 <46707197+amos42@users.noreply.github.com>
2021-03-22 23:38:08 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ cd ~/esp-open-sdk
 make VENDOR_SDK=${SDK_VERSION:-1.5.3}
 
 # change dir to the directory that is linked with the project that is going to be compiled or flashed
-cd ~/shared_project
+cd ~/build
 
 eval $*
